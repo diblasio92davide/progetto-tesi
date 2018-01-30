45c59088e23a24ba0858171b021b3eabfa578628
Davide Diblasio <diblasio.davide92@gmail.com>
2018-01-30 13:23:21 +0100

    Errori spiegati nei commenti.

--- src/main/java/coreNLP/TestCoreNLP.java
+++ src/main/java/coreNLP/TestCoreNLP.java
@@ -9,21 +9,94 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.json.JSONException;
+import org.json.simple.parser.ParseException;
 
 import utility.JsonUtils;
 import utility.TagmeUtils;
 
 public class TestCoreNLP {
 
-    public static void main(String[] args) throws IOException, JSONException {
+    public static void main(String[] args) throws IOException {
 
         StanfordCoreNLP pipeline = new StanfordCoreNLP(
                 PropertiesUtils.asProperties(
                         "annotators", "tokenize,ssplit,pos,lemma,ner",
                         "ssplit.isOneSentence", "true",
                         "tokenize.language", "en", "entitySubclassification", "iob2"));
-        BufferedReader reader = new BufferedReader(new FileReader((args[0])));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+        BufferedWriter writer1 = new BufferedWriter(new FileWriter(args[2]));
+        
+        /*Argomenti passati al main in questo ordine:
+         * 0 - "Input.txt"
+         * 1 - "Entity Type.txt"
+         * 2 - "RHO Fisso.txt"
+         * 3 - "RHO a scelta.txt"
+         * 4 - "items-prova.json"
+         * 5 - "items-1820.json"
+         */
+        
+        
+        /*
+         * Nel codice sottostante ho utilizzato il metodo getBodyProva per estrarre il
+         * contenuto del file prova json relativo al titolo e al body come mi aveva richiesto.
+         * 1° problema - Nonostante stampi bene l'ouput del metodo ner(), questo non lo scrive
+         * 	  nel file Entity Type.txt
+         * 2° problema - Nel metodo rhoFisso di JsonUtils mi solleva un'eccezione riguardante
+         * 	  gli indici di start e end dell'output TagmeUtils.process(textBody): ho stampato
+         * 	  ogni cosa, ma mi sono reso conto che stampando anche gli indici in effetti
+         * 	  stranamente ad un certo punto del ciclo il valore dell'end di un jsonObject
+         * 	  è maggiore dello start del jsonObject successivo.
+         * 3° problema - Ho provato anche la lettura di uno dei file json contenente tutte
+         * 	  le notizie su ogni riga. Per fare ciò il metodo giusto per leggere da questo file
+         *    è getBody() e non getBodyProva(), il motivo l'ho scritto nella classe JsonUtils
+         *    proprio sopra questo metodo. I problemi che riscontro è la grande lentezza nel
+         *    leggere questo file(figuriamoci nella lettura di tutti i file json che mi ha mandato)
+         *    e soprattutto gli errori relativi alla scrittura su file perchè la lunghezza di ogni riga
+         *    dei file json supera i valori consentiti per scrivere su file.
+         */
+    	try {
+    		BufferedReader reader = new BufferedReader(new FileReader((args[4])));
+    		
+    		while(reader.ready())
+    		{
+    			String textDocumentJSON = reader.readLine();
+    			String textBody = JsonUtils.getBodyProva(textDocumentJSON);
+    			Annotation document = new Annotation(textBody);
+                pipeline.annotate(document);
+                Annotator annotator = new Annotator(document);
+                
+                String nerout = annotator.ner();
+                System.out.println("Stampa metodo di stanfordNLP");
+                System.out.println(nerout);
+                writer.write(nerout);
+                writer.newLine();
+                
+                System.out.println("\nStampa del contentuto del body del file JSON");
+                System.out.println(textBody);
+                
+                System.out.println("Stampa output del metodo process di TagmeUtils");
+                System.out.println(TagmeUtils.process(textBody));
+                String rhoFisso = JsonUtils.rhoFisso(textBody, TagmeUtils.process(textBody));
+                writer1.write(rhoFisso);
+                writer1.newLine();
+    		}
+    		
+    		reader.close();
+    		writer.close();
+    		writer1.close();
+            
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	
+    	/*CODICE SCRITTO IN PRECEDENZA ANALIZZANDO IL FILE "Input.txt" FUNZIONANTE
+    	
+    	
+    	/*BufferedReader reader = new BufferedReader(new FileReader((args[0])));
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
         BufferedWriter writer1 = new BufferedWriter(new FileWriter(args[2]));
         BufferedWriter writer2 = new BufferedWriter(new FileWriter(args[3]));
@@ -58,6 +131,8 @@ public class TestCoreNLP {
         writer.close();
         writer1.close();
         writer2.close();
+        */
+        
     }
 
 }
--- src/main/java/utility/JsonUtils.java
+++ src/main/java/utility/JsonUtils.java
@@ -1,16 +1,24 @@
 package utility;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonUtils {
 	
-	public static String rhoFisso(String textDocument, String jsonSTR) throws JSONException, IOException
+	/*
+	 * Questo metodo prende in considerazione Tagme con il valore rho predefinito di 0.1
+	 */
+	public static String rhoFisso(String textDocument, String jsonSTR) throws IOException, ParseException
 	{
-		JSONObject jsonOBJ = new JSONObject(jsonSTR);
+		JSONParser jParser = new JSONParser();
+		Object ob = jParser.parse(jsonSTR);
+		
+		JSONObject jsonOBJ = (JSONObject)ob;
 		int startTag, endTag,
 			start = 0, end = 0;
 		double rho;
@@ -18,21 +26,22 @@ public class JsonUtils {
 		JSONArray annotations = (JSONArray)jsonOBJ.get("annotations");
 		StringBuilder output = new StringBuilder("");
 		
-		for(int i=0; i<annotations.length(); i++)
+		for(int i=0; i<annotations.size(); i++)
 		{
-			startTag = ((JSONObject)annotations.get(i)).getInt("start");
-			endTag = ((JSONObject)annotations.get(i)).getInt("end");
-			rho = ((JSONObject)annotations.get(i)).getDouble("rho");
+			startTag = ((Long)((JSONObject)annotations.get(i)).get("start")).intValue();
+			endTag = ((Long)((JSONObject)annotations.get(i)).get("end")).intValue();
+			rho = (Double)((JSONObject)annotations.get(i)).get("rho");
 			
 			if(startTag > 0)
 			{
 				end = startTag;
+				System.out.println(start + " " + end);
 				output.append(textDocument.substring(start, end));
 			}
 			
 			if(rho >= 0.1)
 			{
-				title = ((JSONObject)annotations.get(i)).getString("title");
+				title = (String)((JSONObject)annotations.get(i)).get("title");
 				output.append(title);
 			}
 			else
@@ -47,9 +56,15 @@ public class JsonUtils {
 		return output.toString();
 	}
 	
-	public static String rhoMinimo(String textDocument, String jsonSTR, double rhoVAR) throws JSONException, IOException
+	/*
+	 * Questo metodo prende in considerazione Tagme con il valore minimo di rho passato in input
+	 */
+	public static String rhoMinimo(String textDocument, String jsonSTR, double rhoVAR) throws IOException, ParseException
 	{
-		JSONObject jsonOBJ = new JSONObject(jsonSTR);
+		JSONParser jParser = new JSONParser();
+		Object ob = jParser.parse(jsonSTR);
+		
+		JSONObject jsonOBJ = (JSONObject)ob;
 		int startTag, endTag,
 			start = 0, end = 0;
 		double rho;
@@ -57,21 +72,22 @@ public class JsonUtils {
 		JSONArray annotations = (JSONArray)jsonOBJ.get("annotations");
 		StringBuilder output = new StringBuilder("");
 		
-		for(int i=0; i<annotations.length(); i++)
+		for(int i=0; i<annotations.size(); i++)
 		{
-			startTag = ((JSONObject)annotations.get(i)).getInt("start");
-			endTag = ((JSONObject)annotations.get(i)).getInt("end");
-			rho = ((JSONObject)annotations.get(i)).getDouble("rho");
+			startTag = ((Long)((JSONObject)annotations.get(i)).get("start")).intValue();
+			endTag = ((Long)((JSONObject)annotations.get(i)).get("end")).intValue();
+			rho = (Double)((JSONObject)annotations.get(i)).get("rho");
 			
 			if(startTag > 0)
 			{
 				end = startTag;
+				System.out.println(start + " " + end);
 				output.append(textDocument.substring(start, end));
 			}
 			
 			if(rho >= rhoVAR)
 			{
-				title = ((JSONObject)annotations.get(i)).getString("title");
+				title = (String)((JSONObject)annotations.get(i)).get("title");
 				output.append(title);
 			}
 			else
@@ -84,5 +100,71 @@ public class JsonUtils {
 			output.append(textDocument.substring(start));
 
 		return output.toString();
+	}
+	
+	/*
+	 * Questo metodo è per la lettura di file json NON di prova, poichè diversamente dal file
+	 * json di prova che mi ha inviato, che tra l'altro ho  modificato mettendo tutto
+	 * sulla stessa riga come avviene nel resto dei json che mi ha inviato, il primo oggetto
+	 * che incontra è un JSONArray e non direttamente un JSONObject come avviene nel file di
+	 * prova
+	 */
+	public static String getBody(String textDocumentJSON) throws FileNotFoundException, IOException, ParseException
+	{
+		JSONParser jParser = new JSONParser();
+		
+		Object ob = jParser.parse(textDocumentJSON);
+		JSONArray jArray = (JSONArray)ob;
+		
+		String text = "";
+		
+		for(int i=0; i<jArray.size(); i++)
+		{
+			String body = "";
+			JSONObject news = (JSONObject)jArray.get(i);
+			JSONArray notizia = (JSONArray)news.get("title");
+			
+			String title = (String)notizia.get(0);
+			
+			JSONArray bodyArray = (JSONArray)news.get("body");
+			for(int j=0; j<bodyArray.size(); j++)
+			{
+				body += bodyArray.get(j);
+			}
+			
+			text += title + " - " + body + "\n";
+		}
+		
+		return text;
+		
+	}
+	
+	/*
+	 * Questo metodo è per la lettura di file json di prova
+	 */
+	public static String getBodyProva(String textDocumentJSON) throws FileNotFoundException, IOException, ParseException
+	{
+		JSONParser jParser = new JSONParser();
+		
+		Object ob = jParser.parse(textDocumentJSON);
+		JSONObject news = (JSONObject)ob;
+		
+		String text = "";
+		String body = "";
+			
+		JSONArray notizia = (JSONArray)news.get("title");
+			
+		String title = (String)notizia.get(0);
+			
+		JSONArray bodyArray = (JSONArray)news.get("body");
+		for(int j=0; j<bodyArray.size(); j++)
+		{
+			body += bodyArray.get(j);
+		}
+			
+		text += title + " - " + body + "\n";
+		
+		return text;
+		
 	}
 }
